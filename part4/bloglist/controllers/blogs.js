0844cd2440bc4b98cee17cd09c44c25032a0c9a8
mntrng <nhat.2.nguyen@aalto.fi>
2020-10-29 00:04:59 +0700

    done 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
@@ -8,15 +9,21 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
     const newData = request.body
+
+    const user =  await User.findById(newData.userId)
+
     const blog = new Blog({
         title  : newData.title,
         author : newData.author,
         url    : newData.url,
-        likes  : newData.likes || 0
+        likes  : newData.likes || 0,
+        user   : user._id
     })
 
-    const result = await blog.save()
-    response.status(201).json(result)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
