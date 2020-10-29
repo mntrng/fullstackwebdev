c2e2222c5078dcf38f01f1217b7c50a2c3cb25ad
mntrng <nhat.2.nguyen@aalto.fi>
2020-10-29 23:51:18 +0700

    done 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -13,9 +13,9 @@ const App = () => {
   const [notification, setNotification] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(initialBlogs =>
-      setBlogs(initialBlogs)
-    )  
+    blogService.getAll()
+               .then(sortedBlogs => setBlogs(sortedBlogs)
+    )
   }, [])
 
   useEffect(() => {
@@ -109,8 +109,8 @@ const App = () => {
             </Togglable>
 
             <h2>Blogs</h2>
-            {blogs.map(blog =>
-              <Blog key={blog.id} blog={blog} handleLike={handleLike}/>
+            {blogs.sort((a, b) => b.likes - a.likes)
+                  .map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} />
             )}
           </div>
       }
