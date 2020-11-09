104aa9deb6c0fb461f2c185d5c7792c1b56e95c8
mntrng <nhat.2.nguyen@aalto.fi>
2020-11-09 09:39:22 +0700

    6.12

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -37,7 +37,7 @@ export const createAnecdote = (content) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
 
   switch (action.type) {
     case 'VOTE':
@@ -55,4 +55,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
