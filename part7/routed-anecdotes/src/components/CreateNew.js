d286ea23c9e2dd7fb807015386267c3737718066
trng <nhat.2.nguyen@aalto.fi>
2020-11-10 13:36:10 +0700

    7.8

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,44 +1,56 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useField } from '../hooks'
 
-const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+const CreateNew = ( {addNew} ) => {
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
     const history = useHistory()
-
+    
     const handleSubmit = (e) => {
       e.preventDefault()
-      props.addNew({
-        content,
-        author,
-        info,
+      addNew({
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
       history.push('/')
+    }
+
+    const resetAll = (e) => {
+      e.preventDefault()
+      content.reset()
+      author.reset()
+      info.reset()
+    }
+
+    const Input = ({reset, ...others}) => {
+      return <input {...others} />
     }
   
     return (
       <div>
         <h2>Create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
+        <form>
           <div>
             Content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <Input name='content' {...content} />
           </div>
           <div>
             Author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <Input name='author' {...author} />
           </div>
           <div>
             URL for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <Input name='info' {...info} />
           </div>
-          <button>Create</button>
+          <button type='submit' onClick={handleSubmit}>Create</button> &nbsp;
+          <button onClick={resetAll}>Reset</button>
         </form>
       </div>
     )
-  
 }
 
 export default CreateNew
