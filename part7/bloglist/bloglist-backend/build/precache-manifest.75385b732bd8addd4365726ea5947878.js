893c6d3f9f40f861d3d030284855d941946240c0
trng <nhat.2.nguyen@aalto.fi>
2020-12-03 14:00:22 +0700

    deployment stuff

---
+++ part7/bloglist/bloglist-backend/build/precache-manifest.75385b732bd8addd4365726ea5947878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2aae27a9ff1707d2fe97086139c0945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98eaf7b13ac8caafd954",
+    "url": "/static/css/main.02acbb7f.chunk.css"
+  },
+  {
+    "revision": "3a220236a528dfe4deaa",
+    "url": "/static/js/2.d6f56ea6.chunk.js"
+  },
+  {
+    "revision": "92030348a26ced4244158f5af7d18a19",
+    "url": "/static/js/2.d6f56ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98eaf7b13ac8caafd954",
+    "url": "/static/js/main.23a16c34.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
