1a1eb59e270fe64f643511cb2f0cb47d4d229c6c
trng <nhat.2.nguyen@aalto.fi>
2020-12-03 09:38:38 +0700

    minor changes

--- part7/bloglist/bloglist-backend/app.js
+++ part7/bloglist/bloglist-backend/app.js
@@ -10,7 +10,6 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
-const testingRouter = require('./controllers/testing')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
@@ -25,12 +24,11 @@ app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use(helmet())
 app.use(logger.logger)
+app.use(express.static('build'))
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-
-app.use('/api/testing', testingRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part7/bloglist/bloglist-backend/index.js
+++ part7/bloglist/bloglist-backend/index.js
@@ -1,10 +1,10 @@
 const http = require('http')
 const app = require('./app')
 const logger = require('./utils/logger')
-const config = require('./utils/config')
 
 const server = http.createServer(app)
+const PORT = process.env.PORT || 3001
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)  
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)  
 })
