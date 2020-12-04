f8dfa1066c4e6c2f1ca45d2883166292abfc99aa
trng <nhat.2.nguyen@aalto.fi>
2020-12-04 14:39:20 +0700

    hehe

--- part9/patientor-backend/src/routes/dist/diagnoses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var diagnoseService_1 = require("../services/diagnoseService");
-var router = express.Router();
-router.get('/', function (_req, res) {
-    res.status(200).send(diagnoseService_1.getAllDiagnoses());
-});
-exports["default"] = router;
--- part9/patientor-backend/src/services/dist/diagnoseService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getAllDiagnoses = void 0;
-var diagnoses_json_1 = require("../data/diagnoses.json");
-exports.getAllDiagnoses = function () {
-    return diagnoses_json_1["default"];
-};
