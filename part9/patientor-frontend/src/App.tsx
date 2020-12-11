0479e58ad0f53888ae35639f5dfdee3525789a1f
trng <nhat.2.nguyen@aalto.fi>
2020-12-11 10:58:58 +0700

    Done 9.17

--- part9/patientor-frontend/src/App.tsx
+++ part9/patientor-frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { useStateValue } from "./state";
 import { Patient } from "./types";
 
 import PatientListPage from "./PatientListPage";
+import PatientInfo from "./PatientInfo";
 
 const App: React.FC = () => {
   const [, dispatch] = useStateValue();
@@ -37,7 +38,8 @@ const App: React.FC = () => {
           </Button>
           <Divider hidden />
           <Switch>
-            <Route path="/" render={() => <PatientListPage />} />
+            <Route path="/patients/:id" component={PatientInfo} />
+            <Route path="/" component={PatientListPage} />
           </Switch>
         </Container>
       </Router>
